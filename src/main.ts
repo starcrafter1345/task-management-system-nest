8e66ac4f01152e8118ae8c3e1638029601119c5d
starcrafter1345 <starcrafter1345@gmail.com>
2025-12-08 16:14:02 +0100

    added cors and helmet

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 import { cleanupOpenApiDoc } from "nestjs-zod";
+import helmet from "helmet";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.use(helmet());
 
   const config = new DocumentBuilder()
     .setTitle("Task management system")
